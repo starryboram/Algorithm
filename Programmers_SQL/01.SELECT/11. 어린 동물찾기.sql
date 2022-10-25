e627122d6b15522da29a1885c752e4b9cf7af47b
starryboram <boramplay@naver.com>
2022-10-25 11:17:42 +0900

    [add] study SQL

---
+++ Programmers_SQL/01.SELECT/11. 어린 동물찾기.sql
@@ -0,0 +1,4 @@
+SELECT animal_id, name
+FROM ANIMAL_INS
+WHERE intake_condition != 'aged';
+--intake_condition은 aged가 아닌 경우를 뜻한다고 한 부분을 참고하여 코드 구현
---
+++ Programmers_SQL/01.SELECT/12. 동물의 아이디와 이름.sql
@@ -0,0 +1,5 @@
+SELECT animal_id, name
+FROM animal_ins
+ORDER BY animal_id;
+
+-- ORDER BY : 순서 정렬할 때 사용
