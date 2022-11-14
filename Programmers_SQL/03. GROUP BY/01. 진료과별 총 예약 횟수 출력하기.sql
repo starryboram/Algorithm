0dd78a13cb5eedc0cd914561254032e1bd88054f
Boram Kim <boramplay93@gmail.com>
2022-11-14 10:06:27 +0900

    [add] study SQL

---
+++ Programmers_SQL/03. GROUP BY/01. 진료과별 총 예약 횟수 출력하기.sql
@@ -0,0 +1,12 @@
+SELECT MCDP_CD AS '진료과코드', COUNT(MCDP_CD) AS '5월예약건수'
+FROM APPOINTMENT
+WHERE DATE(APNT_YMD) BETWEEN '2022-05-01' AND '2022-05-31'
+GROUP BY MCDP_CD
+ORDER BY COUNT(MCDP_CD) ASC, MCDP_CD ASC;
+
+-- APPOINTMENT 테이블에서 : FROM APPOINTMENT
+-- 2022년 5월에 예약한 환자 수: WHERE DATE(APNT_YMD) BETWEEN '2022-05-01' AND '2022-05-31'
+-- 진료과코드 별로 조회: GROUP BY MCDP_CD
+-- 컬럼명은 '진료과 코드', '5월예약건수'로 지정: SELECT MCDP_CD AS '진료과코드', COUNT(MCDP_CD) AS '5월예약건수'
+-- 진료과별 예약한 환자 수를 기준으로 오름차순 정렬: ORDER BY COUNT(MCDP_CD) ASC
+-- 예약한 환자 수가 같다면 진료과 코드를 기준으로 오름차순 정렬: ORDER BY COUNT(MCDP_CD) ASC, MCDP_CD ASC
