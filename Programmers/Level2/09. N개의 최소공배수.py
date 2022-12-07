9daeaa5947638bd8accad6caa9ee25f415d549a4
starryram <boramplay@naver.com>
2022-12-08 08:59:34 +0900

    [add] study Algorithm

--- Programmers/Level2/09. N개의 최소공배수.py
+++ Programmers/Level2/09. N개의 최소공배수.py
@@ -13,6 +13,8 @@ def solution(arr):
 
 
 # 다른 사람 풀이 - 처음보는 함수다.
+# gcd란? a와 b의 최대공약수는 'b'와 'a를 b로 나눈 나머지'의 최대공약수와 같다. 즉, gcd(a,b) = gcd(b, a % b)이다. 
+# 어떤 수와 0의 최대공약수는 자기 자신이다. 즉, gcd(n, 0) = n이다.
 from fractions import gcd
 def nlcm(num):      
     answer = num[0]
@@ -20,3 +22,5 @@ def nlcm(num):
         answer = n * answer / gcd(n, answer)
 
     return answer
+
+# 과연 내가 이 알고리즘을 안 까먹고 사용할 수 있을 것인가....
