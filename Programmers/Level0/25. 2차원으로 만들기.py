ad6435d80e5ef78a207c2a1d180cd389b45dca58
starryram <boramplay@naver.com>
2023-01-16 12:31:32 +0900

    [edit] practice python

--- Programmers/Level0/25. 2차원으로 만들기.py
+++ Programmers/Level0/25. 2차원으로 만들기.py
@@ -17,3 +17,8 @@ def solution(num_list, n):
     for i in range(0, len(num_list), n):
         answer.append(num_list[i:i+n])
     return answer
+
+
+## 잘라서 배열로 저장하기(똑같은 문제)
+def solution(my_str, n):
+    return [my_str[i: i + n] for i in range(0, len(my_str), n)]
