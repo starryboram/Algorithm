255c66cffd8aed164545ac832243c7ef669237f4
starryram <boramplay@naver.com>
2023-01-21 14:32:37 +0900

    [add] practice python

---
+++ Programmers/Level0/30. 치킨 쿠폰.py
@@ -0,0 +1,17 @@
+def solution(chicken):
+    answer = 0
+    while True:
+        if chicken >= 10:
+            answer += chicken//10
+            chicken = chicken//10 + chicken%10
+        else:
+            return answer
+
+# 다른 사람 풀이
+def solution(chicken):
+    return int(chicken*0.11111111111)
+
+# 다른 사람 풀이2
+def solution(chicken):
+    answer = (max(chicken,1)-1)//9
+    return answer
