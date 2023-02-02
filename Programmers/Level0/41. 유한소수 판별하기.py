758a26dede6ea418c44ca035a7a56f32b9a21d05
starryram <boramplay@naver.com>
2023-02-02 21:56:07 +0900

    [add] practice python

---
+++ Programmers/Level0/41. 유한소수 판별하기.py
@@ -0,0 +1,9 @@
+# 문제: https://school.programmers.co.kr/learn/courses/30/lessons/120878
+from math import gcd
+def solution(a, b):
+    b //= gcd(a,b)
+    while b%2==0:
+        b//=2
+    while b%5==0:
+        b//=5
+    return 1 if b==1 else 2
